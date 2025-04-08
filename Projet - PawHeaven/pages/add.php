f13b88c47aee12366ef312f89ec680022852d19f
Dmaury26 <151038515+Dmaury26@users.noreply.github.com>
2025-04-08 08:55:50 +0000

    ajout de variables dans add

--- Projet - PawHeaven/pages/add.php
+++ Projet - PawHeaven/pages/add.php
@@ -2,7 +2,12 @@
 
 include '../inc/cle.php';
 
-
+    $nom = htmlspecialchars($_POST['nom']);
+    $age = htmlspecialchars($_POST['age']);
+    $sex = htmlspecialchars($_POST['sex']);
+    $espece = htmlspecialchars($_POST['espece']);
+    $race = htmlspecialchars($_POST['race']);
+    $description = htmlspecialchars($_POST['description']);
 
 
 $sql = "INSERT INTO animaux (nom,sex,espece,race,description)VALUES ('$nom','$age','$sex','$race','$espece','$description')";
